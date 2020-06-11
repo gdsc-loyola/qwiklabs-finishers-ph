abe97e2635edb0bbbe9fd776ebec8159e2beaf90
Franz T <62831344+Znarfois@users.noreply.github.com>
2020-06-11 23:00:52 +1000

    Update index.js
    Removed console.log() and moved .onclick to .index.html

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,12 +1,7 @@
+const secondaryNav = document.querySelector('.navigation-secondary');
+
+const burger = document.querySelector('.burger')
+
 const navToggle = () => {
-  const secondaryNav = document.querySelector('.navigation-secondary')
-  const burger = document.querySelector('.burger')
-
-  burger.addEventListener('click', () => {
-    console.log(secondaryNav)
-    secondaryNav.classList.toggle('nav-display')
-    console.log('working')
-  })
+  secondaryNav.classList.toggle('nav-display')
 }
-
-navToggle()
