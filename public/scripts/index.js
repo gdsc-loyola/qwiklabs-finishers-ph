2fb5f5c11cb8f913f98a151ff0f9d75bf671a849
tanzellgo <tanzell.go@obf.ateneo.edu>
2020-06-18 14:54:04 +0800

    navbar hotfix

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -2,8 +2,16 @@
 
 var navSecondary = document.querySelector(".navigation-secondary");
 
+window.addEventListener("resize", () => {
+    if(window.innerWidth > 850){
+        navSecondary.style.display = "inline-block";
+    } else {
+        navSecondary.style.display = "none";
+    }
+})
+
 function showMenu(){
-    if(navSecondary.style.display === "none"){
+    if(navSecondary.style.display !== "flex"){
         navSecondary.style.display = "flex";
     } else {
         navSecondary.style.display = "none";
