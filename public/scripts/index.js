f21f06145bdb10fc624b2fd53d76c9f6d7805e5b
Franz Taborlupa <franztaborlupa@gmail.com>
2020-07-06 00:30:46 -0700

    Revert "Merge pull request #26 from dsc-ateneo-de-manila-university/franz-quests-hotfix"
    This reverts commit e45fa5a20debf0c6815b39c41f9549c2e0066686, reversing
    changes made to cd6465f45e39771d54d55788542418fd3cd71a8b.

---
+++ public/scripts/index.js
@@ -0,0 +1,20 @@
+// Burger Button
+
+var navSecondary = document.querySelector(".navigation-secondary");
+
+window.addEventListener("resize", () => {
+    if(window.innerWidth > 850){
+        navSecondary.style.display = "inline-block";
+    } else {
+        navSecondary.style.display = "none";
+    }
+})
+
+function showMenu(){
+    if(navSecondary.style.display !== "flex"){
+        navSecondary.style.display = "flex";
+    } else {
+        navSecondary.style.display = "none";
+    }
+}
+
