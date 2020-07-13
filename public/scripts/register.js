ee6b17bcbff07cee129605fb60f88a9b28d513fa
Franz Taborlupa <franztaborlupa@gmail.com>
2020-07-13 08:45:58 -0700

    Hotfix for modal
    Added check icon
    Changed alt text from "checkmark" to "green check icon"
    Added missing js code
    Added .modal that was missing in CSS

--- public/scripts/register.js
+++ public/scripts/register.js
@@ -4,18 +4,25 @@ const questTitle = document.getElementById('quest-title');
 const date = document.getElementById('date');
 const link = document.getElementById('link');
 const submitBtn = document.getElementById('submit-btn');
+var modal = document.querySelector('.modal');
+var registerContainer = document.querySelector('.register-container');
 
-const database = firebase.database();
-const rootRef = database.ref('/finishers');
+// const database = firebase.database();
+// const rootRef = database.ref('/finishers');
 
 submitBtn.addEventListener('click', (e) => {
   e.preventDefault();
-  const autoId = rootRef.push().key
+  
+ /* const autoId = rootRef.push().key
   rootRef.child(autoId).set({
     first_name: firstName.value,
     last_name: lastName.value,
     quest_title: questTitle.value,
     date: date.value,
-    link: link.value
-  });
+    link: link.value 
+  }); */
+
+  modal.style.display = "flex";
+  registerContainer.style.filter = "brightness(70%)";
+
 });
