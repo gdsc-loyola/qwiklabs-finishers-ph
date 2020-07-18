9334a7e28ef4711445f2e4c849f9c777d6be5137
Franz Taborlupa <franztaborlupa@gmail.com>
2020-07-18 08:30:10 -0700

    Minor fixes + register firestore

--- public/scripts/register.js
+++ public/scripts/register.js
@@ -1,28 +1,82 @@
-const firstName = document.getElementById('first-name');
-const lastName = document.getElementById('last-name');
+const name = document.getElementById('full-name');
 const questTitle = document.getElementById('quest-title');
-const date = document.getElementById('date');
-const link = document.getElementById('link');
+const completionDate = document.getElementById('date');
+//const link = document.getElementById('link');
 const submitBtn = document.getElementById('submit-btn');
 var modal = document.querySelector('.modal');
 var registerContainer = document.querySelector('.register-container');
+const image = document.getElementById('img');
+//var item = document.querySelector('input');
 
-// const database = firebase.database();
-// const rootRef = database.ref('/finishers');
 
+
+// Validation of input
+/*var proceed=false;
+
+        function checkName() {
+          //checks if name consists of a-z characters and between 2-20 characters
+          var name=document.getElementById("full-name").value;
+          var check = /^[A-Za-z\s\'\-]{2,20}$/;
+      
+            //output cofirmation or error to "fNameValid"
+            if (check.test(name)) {
+              item.style.border = "4px solid green";
+              //since it's valid, proceed variable is changed to TRUE
+              proceed=true;
+            }
+            else {
+              
+              proceed=false;
+            }
+        }   
+          
+        }*/
+        
 submitBtn.addEventListener('click', (e) => {
+
   e.preventDefault();
+
+    //once everything is done, function checks the variable proceed meaning everything is valid
+    //if (proceed) {
+      //the user then receives an alert to confirm and asked to press OK or CANCEL
+  if (confirm("Confirm?")) {
+    modal.style.display = "flex";
+    registerContainer.style.filter = "brightness(70%)";
+  }
+  else {
+
+  }
+
   
- /* const autoId = rootRef.push().key
+ db.put(image.name)
+  .then(function(snapshot) {
+    console.log('Uploaded', snapshot.totalBytes, 'bytes.');
+    
+    let downloadURL = db.getDownloadUrl;
+    });
+    
+
+  db.collection("finishers").add({
+    name: name.value,
+    quest: questTitle.value,
+    completionDate: completionDate.value,
+    image: downloadURL
+
+    
+  })
+  .then(function() {
+    console.log("Document successfully written!");
+  })
+  .catch(function(error) {
+    console.error("Error writing document: ", error);
+  });
+
+  /*const autoId = rootRef.push().key;
   rootRef.child(autoId).set({
-    first_name: firstName.value,
-    last_name: lastName.value,
-    quest_title: questTitle.value,
-    date: date.value,
-    link: link.value 
-  }); */
-
-  modal.style.display = "flex";
-  registerContainer.style.filter = "brightness(70%)";
-
+    name: name.value,
+    quest: questTitle.value,
+    completionDate: completionDate.value,
+    //image: image.value;
+    //link: link.value 
+  });*/
 });
