8bd80f1d2695b0dbbfd8ee5894a8997f2289ea22
Franz Taborlupa <franztaborlupa@gmail.com>
2020-07-23 11:43:25 +0800

    Show finisher image working
    Added .getDownloadUrl
    Need to fix style

--- public/scripts/finishers.js
+++ public/scripts/finishers.js
@@ -1,7 +1,7 @@
 // Firestore >> finishers.html
 
 // rendering function
-const groups = document.querySelector(".finisher-group");
+const groups = document.querySelector(".finisher-group-body");
 
 // Create a reference from a Google Cloud Storage URI
 
@@ -11,38 +11,39 @@ db.collection('finishers').get().then(snapshot => {
         var finisher = doc.data();
         console.log(finisher);
         renderFinisher(finisher);
-        var gsReference = firebase.storage().refFromURL('gs://qwiklabs-finishers-ph-e7667.appspot.com/finishers_imgs/')
-
-        // Create a reference to the file we want to download
-        var finisherRef = firebase.storage().ref('finishers-imgs/')/*.child(finisher.name)*/;
-        const image = finisher.filter(data => finisherRef === data.ref)
-        // Get the download URL
-        finisherRef.getDownloadURL().then(function(url) {
-            finisherImg.src = finisher.image;
-        })
+        
     });
 });
 
 function renderFinisher(finisher) {
     //NOTE: Doesn't include finished quests, only rendering the finisher themself
-    let finisherGroupBody = document.createElement('div');
+    //let finisherGroupBody = document.createElement('div');
     let finisherMember = document.createElement('div');
     let finisherImg = document.createElement('img');
     let finisherName = document.createElement('h5');
     let finisherCompletionDate = document.createElement('p');
 
-    finisherGroupBody.appendChild(finisherMember);
+    //finisherGroupBody.appendChild(finisherMember);
+    //finisherMember.appendChild(finisherImg);
     finisherMember.appendChild(finisherImg);
     finisherMember.appendChild(finisherName);
     finisherMember.appendChild(finisherCompletionDate);
-
-    finisherGroupBody.classList.add("finisher-group-body");
+    
+    //finisherGroupBody.classList.add("finisher-group-body");
     finisherMember.classList.add("finisher-member");
    
     finisherName.textContent = finisher.name;
-    finisherCompletionDate.src = finisher.completionDate;
+    finisherCompletionDate.textContent = finisher.completionDate;
+    var gsReference = firebase.storage().refFromURL('gs://qwiklabs-finishers-ph-e7667.appspot.com/finishers_imgs/')
 
-    
+    // Create a reference to the file we want to download
+    var finisherRef = gsReference.child(finisher.name);
+    //const img = Object.values(finisher).filter(data => finisherRef === data.ref)
+    //Get the download URL
+    finisherRef.getDownloadURL().then(function(url) {
+        console.log(url);
+        finisherImg.src = url;
+    })
 
-        groups.appendChild(finisherGroupBody);
+    groups.appendChild(finisherMember);
 }
